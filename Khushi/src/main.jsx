1d3e273602958bff0cec384b797b1bbf2cc8dab8
gjaayush <gjreema@gmail.com>
2026-09-11 12:17:45 +0530

    vercel problem solved

--- Khushi/src/main.jsx
+++ Khushi/src/main.jsx
@@ -1,24 +1,7 @@
-import * as React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-
-// React 19 global / default export polyfill for Three.js / R3F packages
-if (typeof window !== 'undefined') {
-  window.React = React;
-  if (!React.default) {
-    try {
-      Object.defineProperty(React, 'default', {
-        value: React,
-        enumerable: false,
-        configurable: true,
-      });
-    } catch {
-      React.default = React;
-    }
-  }
-}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
